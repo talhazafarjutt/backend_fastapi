492bc733eb84f89b347c955600996c6b5739463e
Talha Zafar <talha.zafar.j@gmail.com>
2024-02-06 00:15:33 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,25 @@
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-import asyncio
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+engine = create_async_engine(
+    'postgresql+asyncpg://postgres:root@localhost:5432/backup',
+    future=True,
+    echo=True,
+)
+
+# expire_on_commit=False will prevent attributes from being expired
+# after commit.
+AsyncSessionFactory = async_sessionmaker(
+    engine,
+    autoflush=False,
+    expire_on_commit=False,
+)
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:root@localhost:5432/backup"
-
-# Asynchronous SQLAlchemy engine and session setup
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-
-Base = declarative_base()
-
-async def get_db():
-    async with AsyncSessionLocal() as session:
+# Dependency
+async def get_db() -> AsyncGenerator:
+    async with AsyncSessionFactory() as session:
+        # logger.debug(f"ASYNC Pool: {engine.pool.status()}")
         yield session
