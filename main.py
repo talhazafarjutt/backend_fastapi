45789194eded442f7c04f64933b120d9b9a6d500
Talha Zafar <talha.zafar.j@gmail.com>
2024-02-12 00:44:35 +0500

    all perfect

--- main.py
+++ main.py
@@ -19,20 +19,46 @@ app.add_middleware(
 )
 
 @app.get("/api/number/{number}") 
-async def main_route(number: int, db:AsyncSession = Depends(get_session)):
-    entry = None
-    async with db.begin():
-        result = await db.execute(select(YourTable).filter(YourTable.number.cast(BigInteger) == int(number)))
-        print('check result: ',result)
-        entry = result.scalar_one_or_none()
+async def main_route(number: str, db:AsyncSession = Depends(get_session)):
+    try:
+        entry = None
+        async with db.begin():
+            result = await db.execute(select(YourTable).filter(YourTable.number == number))
+            print('check result: ',result)
+            entry = result.scalar_one_or_none()
 
-    if entry is None:
-        raise HTTPException(status_code=404, detail="Item not found")
+        if entry is None:
+            raise HTTPException(status_code=404, detail="Item not found")
 
-    return {"data": {
-        "id": entry.id,
-        "number": entry.number,
-        "name": entry.name,
-        "cnic": entry.cnic,
-        "address": entry.address
-    }}
+        return {"data": {
+            "id": entry.id,
+            "number": entry.number,
+            "name": entry.name,
+            "cnic": entry.cnic,
+            "address": entry.address
+        }}
+    except Exception as error:
+        raise HTTPException(status_code=404, detail=str(error))
+
+
+@app.get("/api/cnic/{cnic}") 
+async def main_route(cnic: str, db:AsyncSession = Depends(get_session)):
+    try:
+        entry = None
+        async with db.begin():
+            result = await db.execute(select(YourTable).filter(YourTable.cnic == cnic))
+            print('check result: ',result)
+            entry = result.scalar_one_or_none()
+
+        if entry is None:
+            raise HTTPException(status_code=404, detail="Item not found")
+
+        return {"data": {
+            "id": entry.id,
+            "number": entry.number,
+            "name": entry.name,
+            "cnic": entry.cnic,
+            "address": entry.address
+        }}
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
